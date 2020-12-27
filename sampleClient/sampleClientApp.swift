a488d509abd8dc6d2cdda8ae80c5010196c1ee0a
SATOSHI NAKAJIMA <satoshi.nakajima@gmail.com>
2020-12-27 11:51:53 -0800

    sampleClient

---
+++ sampleClient/sampleClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleClientApp.swift
+//  sampleClient
+//
+//  Created by SATOSHI NAKAJIMA on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct sampleClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
